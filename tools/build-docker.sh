75ebce95e4c0363805f0f04f79f54e52d2c3c5ec
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2021-04-24 23:05:53 +0200

    Saját felhasználó használata a docker buildbelésnél.

--- tools/build-docker.sh
+++ tools/build-docker.sh
@@ -2,6 +2,6 @@
 
 rm -Rf build
 mkdir build 
-docker build -t aoe2maps -f tools/Dockerfile .
+docker build -t aoe2maps -f tools/Dockerfile --build-arg user=$(id -u) --build-arg group=$(id -g) .
 docker run -v $(pwd)/build:/aoe2maps/build aoe2maps
 cp static/* build
