32947d5ffd893ea0c80250672871069b698c8666
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2021-04-25 09:06:45 +0200

    Megfelelő home mappa használata build-docker.sh-ban.

--- tools/build-docker.sh
+++ tools/build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/env/bin sh
 
 rm -Rf build
-mkdir build 
+mkdir build
 docker build -t aoe2maps -f tools/Dockerfile --build-arg user=$(id -u) --build-arg group=$(id -g) .
-docker run -v $(pwd)/build:/aoe2maps/build aoe2maps
+docker run -v $(pwd)/build:/home/docker-build/aoe2maps/build aoe2maps
 cp static/* build
