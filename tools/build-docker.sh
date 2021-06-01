92c6f81a9eb7ac185caa68523e4debe110163f69
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2021-06-01 22:19:46 +0200

    A build szkriptben is rekurzívan kell másolni.

--- tools/build-docker.sh
+++ tools/build-docker.sh
@@ -4,4 +4,4 @@ rm -Rf build
 mkdir build
 docker build -t aoe2maps -f tools/Dockerfile --build-arg user=$(id -u) --build-arg group=$(id -g) .
 docker run --rm -v /$(pwd)/build:/home/docker-build/aoe2maps/build aoe2maps
-cp static/* build
+cp -R static/* build
